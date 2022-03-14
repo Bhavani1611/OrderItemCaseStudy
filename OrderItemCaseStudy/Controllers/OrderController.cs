41e18ffdd64c4f081c5e5d68223576eef6d41ea5
S <2085254@cognizant.com>
2022-03-14 10:23:48 +0530

    first commit

---
+++ OrderItemCaseStudy/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrderItemCaseStudy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OrderItemCaseStudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetCartBy(int id)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://localhost:44324/api/");
+                var responseTask = client.GetAsync("MenuItem");
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+
+                List<Cart> ltems = new List<Cart>();
+
+
+                if (result.IsSuccessStatusCode)
+                {
+
+                    string jsonData = result.Content.ReadAsStringAsync().Result;
+
+
+                    ltems = JsonConvert.DeserializeObject<List<Cart>>(jsonData);
+                    Cart obj1 = ltems.SingleOrDefault(item => item.Id == id);
+
+                    //Cart obj = new Cart();
+                    obj1.MenuItemId = 1;
+                    obj1.UserId = 1;
+
+
+                    return Ok(obj1);
+
+                }
+                else
+                {
+
+                    return BadRequest();
+
+                }
+
+            };
+
+
+
+        }
+
+    }
+}
